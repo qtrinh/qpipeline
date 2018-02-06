6e8060aba0b5aa5ac4adb3fcc3860f801c60922c
Quang Trinh <quang.trinh@gmail.com>
2018-02-06 10:51:56 -0500

    added scripts/env.sh

---
+++ scripts/env.sh
@@ -0,0 +1,9 @@
+
+QPIPELINE_HOME=$1
+
+echo 
+echo "export QPIPELINE_HOME=$QPIPELINE_HOME"
+
+echo "export PATH=\$PATH:\${QPIPELINE_HOME}:\${QPIPELINE_HOME}/external_tools/samtools/samtools-0.1.18:\${QPIPELINE_HOME}/external_tools/tabix/tabix-0.2.5"
+
+echo 
