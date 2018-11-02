edf6bf801f421438bfbcb76df73e183671d6a580
Quang Trinh <quang.trinh@gmail.com>
2018-11-01 22:57:52 -0400

    updated env.sh to include scripts in the path

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,9 +1,8 @@
 
-QPIPELINE_HOME=$1
 
 echo 
-echo "export QPIPELINE_HOME=$QPIPELINE_HOME"
+echo "export QPIPELINE_HOME=`pwd`"
 
-echo "export PATH=\$PATH:\${QPIPELINE_HOME}:\${QPIPELINE_HOME}/external_tools/samtools/samtools-0.1.18:\${QPIPELINE_HOME}/external_tools/tabix/tabix-0.2.5"
+echo "export PATH=\$PATH:\${QPIPELINE_HOME}:\${QPIPELINE_HOME}/scripts:\${QPIPELINE_HOME}/external_tools/samtools/samtools-0.1.18:\${QPIPELINE_HOME}/external_tools/tabix/tabix-0.2.5"
 
 echo 
